59378724f1531fb96c7e644768eb3940b04a9120
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-11-14 22:33:26 +0100

    prepared for certificates popup

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const popup = document.querySelector('.popup');
+const openPopup = document.querySelector('.certificate-card');
+const popupImage = document.querySelector('.popup-image');
+const popupText = document.querySelector('.popup-text');
+const closePopup = document.querySelector('.close-icon');
