f56d53c061e44046218d0fefda2e7f823978be7e
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-12-08 17:34:15 +0100

    added certificate popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,51 @@
 const popup = document.querySelector('.popup');
-const openPopup = document.querySelector('.certificate-card');
+const certificatePopup = document.querySelectorAll('.certificate-card');
 const popupImage = document.querySelector('.popup-image');
-const popupText = document.querySelector('.popup-text');
-const closePopup = document.querySelector('.close-icon');
+const popupName = document.querySelector('.popup-name');
+const popupSchool = document.querySelector('.popup-school');
+const closePopupButton = document.querySelector('.close-icon');
+
+function openPopup () {
+    popup.classList.add('popup_opened');
+    document.addEventListener('mousedown', closePopupOverlay);
+    document.addEventListener('keydown', closePopupEscape);
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+    document.removeEventListener('mousedown', closePopupOverlay);
+    document.removeEventListener('keydown', closePopupEscape);
+}
+
+const closePopupOverlay = function(evt) {
+    const openPopup = document.querySelector('.popup_opened');
+        if(evt.target === openPopup) {
+            closePopup(openPopup);
+        }
+  }
+  
+const closePopupEscape = function(evt) {
+    if(evt.key === 'Escape') {
+      const openPopup = document.querySelector('.popup_opened');
+      closePopup(openPopup);
+    }
+  }
+
+function certificateData (card) {
+    const image = card.querySelector('.certificate-image');
+    const certificateName = card.querySelector('.certificate-name');
+    const certificateSchool = card.querySelector('.certificate-school');
+
+    popupImage.src = image.src;
+    popupName.textContent = certificateName.textContent;
+    popupSchool.textContent = certificateSchool.textContent;
+    openPopup(certificatePopup);
+}
+
+certificatePopup.forEach(function(card) {
+    card.addEventListener('click', function() {
+      certificateData(card)
+    });
+  });
+
+closePopupButton.addEventListener('click', closePopup);
